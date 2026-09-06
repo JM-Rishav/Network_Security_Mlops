1546a7b11258e06db524a632cb52644365d01cbd
Rishav Sukarna <rishav.morningstar25@gmail.com>
2026-09-06 18:54:43 +0530

    updated exception.py

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -17,6 +17,7 @@ class NetworkSecurityException(Exception):
 
 if __name__ =='__main__':
     try: 
+        logger.logging.info("Enter the try block.")
         a = 1 / 0  # This will raise a ZeroDivisionError
         print("This line will not be executed.",a)
     except NetworkSecurityException as e:
